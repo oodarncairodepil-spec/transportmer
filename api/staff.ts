1ff3bd4f482a2438d8af4d8c9fa63c8db474b96f
OODC <oodarncairodepil@gmail.com>
2026-03-31 17:34:09 +0700

    Add serverless debug wrapper to prevent Vercel function crashes

--- api/staff.ts
+++ api/staff.ts
@@ -1,7 +1,5 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../server/index";
+import { handleWithExpressApp } from "./_app";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req as any, res as any);
+export default function handler(req: any, res: any) {
+  return handleWithExpressApp(req, res);
 }
-
